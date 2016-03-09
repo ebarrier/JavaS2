fc8f3e59f651ea5e30ea8efabbc3c946a06c5fe4
Etienne <etienne.barrier@gmail.com>
2016-03-09 11:48:16 +0200

    Start building session factory

---
+++ Homework5/src/factory/HibernateEngine.java
@@ -0,0 +1,24 @@
+package factory;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;;
+
+public class HibernateEngine {
+
+
+	public class InitSessionFactory {
+	   /** The single instance of hibernate SessionFactory */
+	   private static org.hibernate.SessionFactory sessionFactory;
+	   private InitSessionFactory() {
+	   }
+
+	   static {
+	      final Configuration cfg = new Configuration();
+	      cfg.configure("/hibernate.cfg.xml");
+	      sessionFactory = cfg.buildSessionFactory();
+	   }
+	   public static SessionFactory getInstance() {
+	      return sessionFactory;
+	   }
+	}
+}

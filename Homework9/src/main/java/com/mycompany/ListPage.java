c9e30c5ac6d4c4972af99faa2507d62909750a41
ebarrier <etienne.barrier@gmail.com>
2016-04-11 13:43:45 +0300

    Init commit. Basic pages for homework.

---
+++ Homework9/src/main/java/com/mycompany/ListPage.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class ListPage extends WebPage {
+
+	public ListPage(final PageParameters parameters) {
+		super(parameters);
+
+    }
+}

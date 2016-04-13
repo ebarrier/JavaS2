1f8bf12d642a5dd7aaf2a9fdd8a0a046bfe6d39b
ebarrier <etienne.barrier@gmail.com>
2016-04-13 17:12:19 +0300

    Right commit with working list insertion

--- Homework9/src/main/java/com/mycompany/ListPage.java
+++ Homework9/src/main/java/com/mycompany/ListPage.java
@@ -1,29 +1,47 @@
 package com.mycompany;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.util.value.ValueMap;
 
 public class ListPage extends WebPage {
-
-	private ArrayList<String> list = new ArrayList<>();
+	
+	private List<String> itemList = new ArrayList<String>();
 	
 	public ListPage(final PageParameters parameters) {
 		super(parameters);
-		
 		add(new ItemForm("itemForm"));
-		
-//		add(new Link("add") {
-//
-//			@Override
-//			public void onClick() {
-//				list.add(input);
-//			}
-//		});
-
+		add(new ListView("items", itemList) {
+			@Override
+			protected void populateItem(ListItem item) {
+				item.add(new Label("text", item.getModel()));
+			}
+		});
     }
+	
+	public class ItemForm extends Form<ValueMap> { 
+
+		public ItemForm(String id) {
+			super(id, new CompoundPropertyModel<ValueMap>(new ValueMap()));		
+			add(new TextField("itemField").setType(String.class));
+		}
+		
+		@Override
+	    public final void onSubmit() {
+			ValueMap values = getModelObject();
+			String itemText = values.getString("itemField");
+			itemList.add(0, itemText);
+			values.put("itemField", "");
+		}
+	}
 }

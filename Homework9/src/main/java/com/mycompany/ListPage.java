33e55e895ada21bbcc53bf0b1a0de66ff5479150
ebarrier <etienne.barrier@gmail.com>
2016-04-13 18:11:55 +0300

    Try add remove button but not working

--- Homework9/src/main/java/com/mycompany/ListPage.java
+++ Homework9/src/main/java/com/mycompany/ListPage.java
@@ -3,31 +3,45 @@ package com.mycompany;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.util.value.ValueMap;
 
 public class ListPage extends WebPage {
 	
 	private List<String> itemList = new ArrayList<String>();
+	private Model itemListModel = new Model<>();
 	
 	public ListPage(final PageParameters parameters) {
 		super(parameters);
+		
 		add(new ItemForm("itemForm"));
 		add(new ListView("items", itemList) {
 			@Override
 			protected void populateItem(ListItem item) {
 				item.add(new Label("text", item.getModel()));
-			}
+//				for (String s : itemList) {
+//					add(new Link("removeItem") {						
+//						@Override
+//						public void onClick() {
+//							System.out.println(item.getIndex());
+//						}
+//					});
+//				}
+			}	
 		});
     }
+	
 	
 	public class ItemForm extends Form<ValueMap> { 
 
@@ -40,7 +54,7 @@ public class ListPage extends WebPage {
 	    public final void onSubmit() {
 			ValueMap values = getModelObject();
 			String itemText = values.getString("itemField");
-			itemList.add(0, itemText);
+			itemList.add(itemText);
 			values.put("itemField", "");
 		}
 	}

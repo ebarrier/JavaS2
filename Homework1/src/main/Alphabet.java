a1de6f27bc9b25ea73c05c846f3490a3a2a4b822
ebarrier <etienne.barrier@gmail.com>
2016-04-30 14:19:34 +0300

    Homework1 done

--- Homework1/src/main/Alphabet.java
+++ Homework1/src/main/Alphabet.java
@@ -4,16 +4,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Alphabet {
+	
+	private static ArrayList<String> temp = new ArrayList<String>();
 
 	public static void main(String[] args) {
 		System.out.println("Please enter a text");
 		String input = TextIO.getln();
+		input.toLowerCase();
 
-		ArrayList<String> temp = new ArrayList();
-		String[][] outer = new String[5][input.length()];
+		String[] outer = new String[5];
+		for (int i = 0; i < outer.length; i++) {
+			outer[i] = "";
+		}
+		
+		processInput(input);
 
-		for (int j = 0; j < input.length(); j++) {
-			char c = input.charAt(j);
+		for (int i = 0; i < 5; i++) {
+			for (int k = 0; k < temp.size(); k++) {
+				if ((k % 5) == i) {
+					outer[i] = outer[i] + " " + temp.get(k);
+				}
+			}
+			System.out.println(outer[i]);
+		}
+	}
+
+	private static void processInput(String s) {
+
+		for (int j = 0; j < s.length(); j++) {
+			char c = s.charAt(j);
 			if (c == 'a') {
 				temp.add(" ### ");
 				temp.add("#   #");
@@ -21,9 +40,181 @@ public class Alphabet {
 				temp.add("#   #");
 				temp.add("#   #");
 			}
-			for (int i = 0; i < 5; i++) {
-				outer[i][j] = temp.get(i);
-				System.out.println(Arrays.deepToString(outer));
+
+			if (c == 'b') {
+				temp.add("#### ");
+				temp.add("#   #");
+				temp.add("#####");
+				temp.add("#   #");
+				temp.add("#### ");
+			}
+			if (c == 'c') {
+				temp.add(" ####");
+				temp.add("#    ");
+				temp.add("#    ");
+				temp.add("#    ");
+				temp.add(" ####");
+			}
+			if (c == 'd') {
+				temp.add("#### ");
+				temp.add("#   #");
+				temp.add("#   #");
+				temp.add("#   #");
+				temp.add("#### ");
+			}
+			if (c == 'e') {
+				temp.add("#####");
+				temp.add("#    ");
+				temp.add("###  ");
+				temp.add("#    ");
+				temp.add("#####");
+			}
+			if (c == 'f') {
+				temp.add("#####");
+				temp.add("#    ");
+				temp.add("###  ");
+				temp.add("#    ");
+				temp.add("#    ");
+			}
+			if (c == 'g') {
+				temp.add(" ### ");
+				temp.add("#    ");
+				temp.add("#  ##");
+				temp.add("#   #");
+				temp.add(" ### ");
+			}
+			if (c == 'h') {
+				temp.add("#   #");
+				temp.add("#   #");
+				temp.add("#####");
+				temp.add("#   #");
+				temp.add("#   #");
+			}
+			if (c == 'i') {
+				temp.add("#");
+				temp.add(" ");
+				temp.add("#");
+				temp.add("#");
+				temp.add("#");
+			}
+			if (c == 'j') {
+				temp.add("  #");
+				temp.add("  #");
+				temp.add("  #");
+				temp.add("  #");
+				temp.add("###");
+			}
+			if (c == 'k') {
+				temp.add("#   #");
+				temp.add("#  # ");
+				temp.add("###  ");
+				temp.add("#  # ");
+				temp.add("#   #");
+			}
+			if (c == 'l') {
+				temp.add("#    ");
+				temp.add("#    ");
+				temp.add("#    ");
+				temp.add("#    ");
+				temp.add("#####");
+			}
+			if (c == 'm') {
+				temp.add("#   #");
+				temp.add("## ##");
+				temp.add("# # #");
+				temp.add("#   #");
+				temp.add("#   #");
+			}
+			if (c == 'n') {
+				temp.add("#   #");
+				temp.add("##  #");
+				temp.add("# # #");
+				temp.add("#  ##");
+				temp.add("#   #");
+			}
+			if (c == 'o') {
+				temp.add(" ### ");
+				temp.add("#   #");
+				temp.add("#   #");
+				temp.add("#   #");
+				temp.add(" ### ");
+			}
+			if (c == 'p') {
+				temp.add("#### ");
+				temp.add("#   #");
+				temp.add("#### ");
+				temp.add("#    ");
+				temp.add("#    ");
+			}
+			if (c == 'q') {
+				temp.add(" ### ");
+				temp.add("#   #");
+				temp.add("#   #");
+				temp.add("#  ##");
+				temp.add(" ####");
+			}
+			if (c == 'r') {
+				temp.add("#### ");
+				temp.add("#   #");
+				temp.add("#### ");
+				temp.add("#  # ");
+				temp.add("#   #");
+			}
+			if (c == 's') {
+				temp.add(" ####");
+				temp.add("#    ");
+				temp.add("  #  ");
+				temp.add("    #");
+				temp.add("#### ");
+			}
+			if (c == 't') {
+				temp.add("#####");
+				temp.add("  #  ");
+				temp.add("  #  ");
+				temp.add("  #  ");
+				temp.add("  #  ");
+			}
+			if (c == 'u') {
+				temp.add("#   #");
+				temp.add("#   #");
+				temp.add("#   #");
+				temp.add("#   #");
+				temp.add(" ### ");
+			}
+			if (c == 'v') {
+				temp.add("#     #");
+				temp.add("#     #");
+				temp.add(" #   # ");
+				temp.add("  # #  ");
+				temp.add("   #   ");
+			}
+			if (c == 'w') {
+				temp.add("#     #     #");
+				temp.add("#     #     #");
+				temp.add(" #   # #   # ");
+				temp.add("  # #   # #  ");
+				temp.add("   #     #   ");
+			}
+			if (c == 'x') {
+				temp.add("#   #");
+				temp.add(" # # ");
+				temp.add("  #  ");
+				temp.add(" # # ");
+				temp.add("#   #");
+			}
+			if (c == 'y') {
+				temp.add("#   #");
+				temp.add(" # # ");
+				temp.add("  #  ");
+				temp.add("  #  ");
+				temp.add("  #  ");
+			}
+			if (c == 'z') {
+				temp.add("#####");
+				temp.add("   # ");
+				temp.add("  #  ");
+				temp.add(" #   ");
+				temp.add("#####");
 			}
 		}
 	}

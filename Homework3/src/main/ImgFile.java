92500a7763884f990ca9e0faee408f6196570135
ebarrier <etienne.barrier@gmail.com>
2016-05-01 11:52:51 +0300

    Separate methods

--- Homework3/src/main/ImgFile.java
+++ Homework3/src/main/ImgFile.java
@@ -7,57 +7,49 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-//BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);
-//Graphics g = img.getGraphics();
-//g.drawString(input, 5, 95);
-//
-//for (int y = 0; y < height; y++) {
-//	StringBuilder sb = new StringBuilder();
-//	for (int x = 0; x < width; x++) {
-//		sb.append(img.getRGB(x, y) == -16777216 ? " " : img.getRGB(x, y) == -1 ? "#" : "*");
-//	}
-//	if (sb.toString().trim().isEmpty()) continue;
-//	System.out.println(sb);
-
 public class ImgFile {
-	private static final int WIDTH = 50;
-	private static final int HEIGHT = 50;
+	
 	private static BufferedImage origImg;
 	private static BufferedImage resImg;
-	
+
 	public static void main(String[] args) throws IOException {
 		
-		File file = new File("src/resources/Heart_Filled-128.png");
+		loadImage("src/resources/Heart_Filled-128.png");
 		
+		resizeImg(origImg, 50,50);
+		
+		sysoutImg(resImg);
+	}
+	
+	public static BufferedImage loadImage(String path) {
+		File file = new File(path);
 		try {
-		origImg = ImageIO.read(file);
-		resImg = new BufferedImage(WIDTH, HEIGHT, origImg.getType());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		Graphics2D g = resImg.createGraphics();
-		g.drawImage(origImg, 0, 0, WIDTH, HEIGHT, null);
-		g.dispose();
-		
-		
-		for (int y = 0; y < origImg.getHeight(); y++) {
-			StringBuilder sb = new StringBuilder();
-			for (int x = 0; x < origImg.getWidth(); x++) {
-				sb.append(origImg.getRGB(x, y) == -16777216 ? "#" : origImg.getRGB(x, y) == -1 ? " " : "*");
+			origImg = ImageIO.read(file);
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			if (sb.toString().trim().isEmpty()) continue;
-			System.out.println(sb);
-		}
-		
-		for (int y = 0; y < resImg.getHeight(); y++) {
+		return origImg;
+	}
+
+	
+	public static BufferedImage resizeImg(BufferedImage oImg, int width, int height) {
+		resImg = new BufferedImage(width, height, oImg.getType());
+		Graphics2D g = resImg.createGraphics();
+		g.drawImage(oImg, 0, 0, width, height, null);
+		g.dispose();
+		return resImg;
+	}
+	
+	public static void sysoutImg(BufferedImage img) {
+		for (int y = 0; y < img.getHeight(); y++) {
 			StringBuilder sb = new StringBuilder();
-			for (int x = 0; x < resImg.getWidth(); x++) {
-				sb.append(resImg.getRGB(x, y) == -16777216 ? "#" : resImg.getRGB(x, y) == -1 ? " " : "*");
+			for (int x = 0; x < img.getWidth(); x++) {
+				sb.append(img.getRGB(x, y) == -16777216 ? "#" : img.getRGB(x, y) == -1 ? " " : "*");
 			}
 			if (sb.toString().trim().isEmpty()) continue;
 			System.out.println(sb);
 		}
 	}
+	
 
 }

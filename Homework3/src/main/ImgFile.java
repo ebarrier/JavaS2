04ab4d213e4c727264c09d5b99efd80342a31252
ebarrier <etienne.barrier@gmail.com>
2016-05-01 11:18:42 +0300

    Add resize image loop

--- Homework3/src/main/ImgFile.java
+++ Homework3/src/main/ImgFile.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -19,21 +20,40 @@ import javax.imageio.ImageIO;
 //	System.out.println(sb);
 
 public class ImgFile {
+	private static final int WIDTH = 50;
+	private static final int HEIGHT = 50;
+	private static BufferedImage origImg;
+	private static BufferedImage resImg;
 	
 	public static void main(String[] args) throws IOException {
 		
 		File file = new File("src/resources/Heart_Filled-128.png");
-		BufferedImage img = null;
+		
 		try {
-		    img = ImageIO.read(file);
+		origImg = ImageIO.read(file);
+		resImg = new BufferedImage(WIDTH, HEIGHT, origImg.getType());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		for (int y = 0; y < img.getHeight(); y++) {
+		Graphics2D g = resImg.createGraphics();
+		g.drawImage(origImg, 0, 0, WIDTH, HEIGHT, null);
+		g.dispose();
+		
+		
+		for (int y = 0; y < origImg.getHeight(); y++) {
 			StringBuilder sb = new StringBuilder();
-			for (int x = 0; x < img.getWidth(); x++) {
-				sb.append(img.getRGB(x, y) == -16777216 ? "#" : img.getRGB(x, y) == -1 ? " " : "*");
+			for (int x = 0; x < origImg.getWidth(); x++) {
+				sb.append(origImg.getRGB(x, y) == -16777216 ? "#" : origImg.getRGB(x, y) == -1 ? " " : "*");
+			}
+			if (sb.toString().trim().isEmpty()) continue;
+			System.out.println(sb);
+		}
+		
+		for (int y = 0; y < resImg.getHeight(); y++) {
+			StringBuilder sb = new StringBuilder();
+			for (int x = 0; x < resImg.getWidth(); x++) {
+				sb.append(resImg.getRGB(x, y) == -16777216 ? "#" : resImg.getRGB(x, y) == -1 ? " " : "*");
 			}
 			if (sb.toString().trim().isEmpty()) continue;
 			System.out.println(sb);
